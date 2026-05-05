42bb0d559a1d5f83d4e5dfbae79693451cd1b214
venkatesh <gunturivenkatesh45@gmail.com>
2026-05-05 12:15:36 +0530

    Today, I have done Collatz and FizzBuzz problems

---
+++ Collatz41.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems100Solving
+{
+    internal class Collatz41
+    {
+        public static string GetCollatzSequence(int num)
+        {
+            if(num<0)
+            {
+                return "Error";
+            }
+            //if(num==0)
+            //{
+            //    return $"{num} 0";
+            //}
+            string str=num + " ";
+            if(num>0)
+            {
+                for(int i=num-1;i>=0;i--)
+                {
+                    str += Math.Pow(2, i)+" ";
+                }
+            }
+            return str;
+        }
+
+        static void Main(string[] args)
+        {
+            int num=int.Parse(Console.ReadLine());
+            Console.WriteLine(GetCollatzSequence(num));
+        }
+    }
+}
---
+++ FizzBizz42.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems100Solving
+{
+    internal class FizzBizz42
+    {
+        public static string GetFizzBizz(int num)
+        {
+            if(num<= 0)
+            {
+                return "Error";
+            }
+            if(num%3==0 && num%5==0)
+            {
+                return "FIZZBIZZ";
+            }
+            if( num%3==0)
+            {
+                return "FIZZ";
+            }
+            if( num%5==0)
+            {
+                return "BIZZ";
+            }
+            
+            return $"{num}";
+        }
+        static void Main(string[] args)
+        {
+            int num=int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFizzBizz(num));
+        }
+    }
+}
